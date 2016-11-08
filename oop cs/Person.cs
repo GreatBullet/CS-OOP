755b8a9da9bab3008a56ed02735a2480153272d5
GreatBullet <rick.armbruster@hotmail.de>
2016-11-08 18:02:46 +0100

    Add project files.

---
+++ oop cs/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_cs
+{
+    public class Person
+    {
+        public Person(string x, string y)
+        {
+            Vorname = x;
+            Nachname = y;
+            Energie = 100;
+        }
+        
+        public string Nachname;
+        public string Vorname;
+        public int Alter;
+        public int Energie;
+        public bool HatHunger;
+
+        public void Laufe(int strecke)
+        {
+            Energie = Energie - strecke;
+        }
+    }
+}
---
+++ oop cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person POP = new Person("Barney", "Stinson");
+            while (!POP.HatHunger)
+            {
+                Console.WriteLine(POP.Energie);
+                POP.Laufe(50);
+                if (POP.Energie <= 0)
+                    POP.HatHunger = true;
+            }
+
+            Console.WriteLine(POP.Energie);
+            Console.ReadKey();
+        }
+    }
+}
